2d2bad219d788125cb36dc2a6ae262222952c038
Tushar <tsinghal299@gmail.com>
2024-11-05 12:53:26 +0530

    first commit

---
+++ loopsdemo.js
@@ -0,0 +1,64 @@
+
+
+
+/*
+if(){
+}
+else if(){
+}
+switch(){
+}
+for(){
+}
+
+*/
+ /*for(let i=1;i<=10;i=i+1){
+    console.log("tushar";i);
+    
+}*/
+
+/*for(let i=10;i<=15;i=i+2){
+    console.log("tushar");
+    
+}*/
+/*for(let i=1;i<=10;i=i+1){
+    console.log("vikas",i);
+    
+}*/
+/*
+for(let i=1,i<=10,i=i+1){
+    console.log("2"+"*"+i+"="+2*i);
+    
+}
+*/
+//for (let row=1,row<=5,row++){
+   // for(let star=1,star<=row,star++){
+    //process.stdout.write("*");
+    //}
+    //console.log();
+    
+//}
+
+// let nsp=3;
+// let nst=1;
+
+// for(let row=1;row<=4;row++){
+//     for(let sp=1;sp<=nsp;sp++){
+//         process.stdout.write(" ")
+//     }
+//     for(let st=1;st<=nst;st++){
+//         process.stdout.write("*")
+//     }
+//     console.log();
+
+//     nsp=nsp-1;
+//     nst=nst+2;
+    
+// }
+
+
+let names =  ["Saksham","Anurag thiland","Lokesh","Raj"];
+    for(let i=0;i<=names.length;i++){
+            console.log(names[1]);
+        }
+    
